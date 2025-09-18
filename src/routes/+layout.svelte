0b51ff13bf9f1f152f4d72c99eb26001d524bd8d
Oliver Rose <hello@olivermrose.com>
2025-09-17 22:06:21 -0400

    feat: define initial layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,6 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<main class="px-4 sm:px-6">
+	{@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Download from '@lucide/svelte/icons/download';
+	import Monitor from '@lucide/svelte/icons/monitor';
+	import Smartphone from '@lucide/svelte/icons/smartphone';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="grid-auto grid gap-6">
+	{#each { length: 30 }}
+		<div class="flex flex-col gap-2">
+			<img
+				class="aspect-video"
+				src="https://placehold.co/3840x2160"
+				alt=""
+				width="1280"
+				height="720"
+			/>
+
+			<div class="flex items-center justify-between">
+				<div class="flex flex-col">
+					<span class="font-semibold">Album name</span>
+					<span class="text-sm text-muted-foreground">Artist name</span>
+				</div>
+
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger>
+						{#snippet child({ props })}
+							<Button size="icon" variant="outline" {...props}>
+								<Download class="size-4" />
+							</Button>
+						{/snippet}
+					</DropdownMenu.Trigger>
+
+					<DropdownMenu.Content>
+						<DropdownMenu.Sub>
+							<DropdownMenu.SubTrigger>
+								<Smartphone /> Mobile
+							</DropdownMenu.SubTrigger>
+
+							<DropdownMenu.SubContent>
+								<!-- Other -->
+								<DropdownMenu.Item>4K Resolution</DropdownMenu.Item>
+							</DropdownMenu.SubContent>
+						</DropdownMenu.Sub>
+
+						<DropdownMenu.Sub>
+							<DropdownMenu.SubTrigger>
+								<Monitor /> Desktop
+							</DropdownMenu.SubTrigger>
+
+							<DropdownMenu.SubContent>
+								<!-- Other -->
+								<DropdownMenu.Item>4K Resolution</DropdownMenu.Item>
+							</DropdownMenu.SubContent>
+						</DropdownMenu.Sub>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.grid-auto {
+		grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+	}
+</style>
