4cb4c37d9240ea6ca1ff92fc567b9bf7e2b438ff
Oliver Rose <hello@olivermrose.com>
2026-03-20 01:49:48 -0400

    fix: make hero responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,10 +22,10 @@
 	});
 </script>
 
-<section id="hero" class="relative aspect-2/1 overflow-hidden bg-neutral-950">
+<section id="hero" class="relative overflow-hidden bg-neutral-950 max-md:grid md:aspect-2/1">
 	{#key heroIndex}
 		<img
-			class="absolute inset-0 h-full w-full object-cover"
+			class="inset-0 col-start-1 row-start-1 h-full w-full object-cover md:absolute"
 			src="{R2_PUBLIC_URL}/{heroWallpaper.slug}.avif"
 			alt=""
 			fetchpriority="high"
@@ -35,11 +35,18 @@
 		/>
 	{/key}
 
-	<div class="absolute inset-0 bg-linear-to-r from-black via-black/50 to-black/10"></div>
+	<div
+		class={[
+			"inset-0 z-10 col-start-1 row-start-1 bg-linear-to-b from-transparent to-neutral-950",
+			"md:absolute md:bg-linear-to-tr md:from-neutral-950 md:via-black/50 md:to-transparent",
+		]}
+	></div>
 
-	<div class="relative flex h-full items-end p-12">
+	<div class="relative z-20 flex h-full items-end p-8 md:p-12">
 		<hgroup class="max-w-3xl">
-			<h1 class="text-6xl font-normal tracking-tight text-neutral-50">Soundtrack your setup.</h1>
+			<h1 class="text-5xl font-normal tracking-tight text-neutral-50 md:text-6xl">
+				Soundtrack your setup.
+			</h1>
 
 			<p class="mt-3 font-mono text-lg text-neutral-300/80">
 				Visualize your taste. Extended cover arts as wallpapers, free to download.
